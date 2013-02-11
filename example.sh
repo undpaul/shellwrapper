7a5a92930a93f0c0ab61c59a7e32c9954a4ddfdb
Johannes Haseitl <johannes@undpaul.de>
2013-02-11 10:56:57 +0100

    Normalize the command to be executed relative to the example.sh file location.

--- example.sh
+++ example.sh
@@ -1,4 +1,7 @@
 
+# Normalize by relocating to the current scripts folder.
+cd "$(dirname ${0})"
+
 # This is the simple call that uses the shellwrapper to process all scripts
 # in the example folder.
 sh shellwrapper.sh ./example local
